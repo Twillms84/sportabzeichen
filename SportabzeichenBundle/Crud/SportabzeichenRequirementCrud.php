4c9e39477b3c4e151ed23458e72fffbf67e5044a
Twillms84 <timo.willms@gmail.com>
2025-12-11 00:18:41 +0100

    final upload csv

--- SportabzeichenBundle/Crud/SportabzeichenRequirementCrud.php
+++ SportabzeichenBundle/Crud/SportabzeichenRequirementCrud.php
@@ -6,6 +6,7 @@ namespace PulsR\SportabzeichenBundle\Crud;
 
 use IServ\CrudBundle\Crud\ServiceCrud;
 use IServ\CrudBundle\Mapper\ListMapper;
+use IServ\CrudBundle\Mapper\FilterMapper;
 use PulsR\SportabzeichenBundle\Entity\SportabzeichenRequirement;
 
 class SportabzeichenRequirementCrud extends ServiceCrud
@@ -14,25 +15,70 @@ class SportabzeichenRequirementCrud extends ServiceCrud
 
     protected function configure(): void
     {
-        $this->title = _('Anforderungskatalog');
+        $this->title = _('Anforderungen');
         $this->itemTitle = _('Anforderung');
 
-        // CSV-Import ist Master → Bearbeiten deaktivieren
-        $this->disableAdd();
-        $this->disableEdit();
-        $this->disableDelete();
+        // *** CSV-Import steuert die Daten → keine CRUD-Aktionen ***
+        $this->canAdd = false;
+        $this->canEdit = false;
+        $this->canDelete = false;
     }
 
+    /**
+     * Felder für die Listenansicht
+     */
     public function configureListFields(ListMapper $list): void
     {
         $list
-            ->addIdentifier('discipline.name', _('Disziplin'))
-            ->add('jahr')
-            ->add('altersklasse')
-            ->add('geschlecht')
-            ->add('bronze')
-            ->add('silber')
-            ->add('gold')
-            ->add('schwimmnachweis');
+            ->add('discipline.name', null, [
+                'label' => _('Disziplin')
+            ])
+            ->add('discipline.kategorie', null, [
+                'label' => _('Kategorie')
+            ])
+            ->add('jahr', null, [
+                'label' => _('Jahr')
+            ])
+            ->add('altersklasse', null, [
+                'label' => _('Altersklasse')
+            ])
+            ->add('geschlecht', null, [
+                'label' => _('Geschlecht')
+            ])
+            ->add('bronze', null, [
+                'label' => _('Bronze')
+            ])
+            ->add('silber', null, [
+                'label' => _('Silber')
+            ])
+            ->add('gold', null, [
+                'label' => _('Gold')
+            ])
+            ->add('schwimmnachweis', null, [
+                'label' => _('Schwimmnachweis')
+            ]);
+    }
+
+    /**
+     * Filter für die Tabelle
+     */
+    public function configureFilterFields(FilterMapper $filter): void
+    {
+        $filter
+            ->add('jahr', null, [
+                'label' => _('Jahr')
+            ])
+            ->add('geschlecht', null, [
+                'label' => _('Geschlecht')
+            ])
+            ->add('altersklasse', null, [
+                'label' => _('Altersklasse')
+            ])
+            ->add('discipline.kategorie', null, [
+                'label' => _('Kategorie')
+            ])
+            ->add('discipline.name', null, [
+                'label' => _('Disziplin')
+            ]);
     }
 }
