52608aa2661837f2cbb762f920cfb0cf59d6f635
Twillms84 <timo.willms@gmail.com>
2025-12-11 15:27:33 +0100

    .

--- SportabzeichenBundle/Controller/ExamResultController.php
+++ SportabzeichenBundle/Controller/ExamResultController.php
@@ -27,7 +27,7 @@ class ExamResultController extends AbstractPageController
         ORDER BY exam_year DESC, exam_date DESC
     ");
     
-    return $this->render('@PulsRSportabzeichen/results/exam_list.html.twig', [
+    return $this->render('@PulsRSportabzeichen/results/index.html.twig', [
     'exams' => $exams,
     'title' => 'Prüfung auswählen',
     ]);
